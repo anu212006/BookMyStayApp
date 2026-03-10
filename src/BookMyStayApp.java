6d4578008ceae2c34d13f90b51347d2de6074d19
anu212006 <anushkaverma212006@gmail.com>
2026-03-10 15:27:13 +0530

    UC6: Implement room allocation and prevent double booking

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,31 +1,60 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Book My Stay Application
- * Use Case 5 - Booking Request Queue
+ * Use Case 6 - Room Allocation & Confirmation
  *
- * @version 5.0
+ * @version 6.0
  */
 
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Book My Stay - Booking Requests");
+        System.out.println("Book My Stay - Room Allocation");
 
+        // Inventory setup
+        HashMap<String, Integer> inventory = new HashMap<>();
+        inventory.put("Single Room", 2);
+        inventory.put("Double Room", 1);
+
+        // Booking request queue
         Queue<String> bookingQueue = new LinkedList<>();
-
-        // Guests submitting booking requests
         bookingQueue.add("Guest1 - Single Room");
         bookingQueue.add("Guest2 - Double Room");
-        bookingQueue.add("Guest3 - Suite Room");
+        bookingQueue.add("Guest3 - Single Room");
 
-        System.out.println("\nBooking Queue:");
+        // Set to store allocated room IDs
+        Set<String> allocatedRooms = new HashSet<>();
 
-        for (String request : bookingQueue) {
+        int roomId = 101;
 
-            System.out.println(request);
+        while (!bookingQueue.isEmpty()) {
+
+            String request = bookingQueue.poll();
+
+            String[] parts = request.split(" - ");
+            String guest = parts[0];
+            String roomType = parts[1];
+
+            if (inventory.containsKey(roomType) && inventory.get(roomType) > 0) {
+
+                String roomNumber = roomType.substring(0,1) + roomId;
+
+                allocatedRooms.add(roomNumber);
+
+                inventory.put(roomType, inventory.get(roomType) - 1);
+
+                System.out.println("Reservation Confirmed for " + guest +
+                        " | Room ID: " + roomNumber);
+
+                roomId++;
+
+            } else {
+
+                System.out.println("No rooms available for " + guest);
+
+            }
 
         }
 
