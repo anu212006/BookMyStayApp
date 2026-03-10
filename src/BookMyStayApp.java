7a1e7622b18b5ffd76f7f05df811d99988e45289
anu212006 <anushkaverma212006@gmail.com>
2026-03-10 14:39:03 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,19 @@
+/**
+ * Book My Stay Application
+ * Entry point for Hotel Booking System
+ *
+ * @author Student
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version 1.0");
+
+    }
+
+}
